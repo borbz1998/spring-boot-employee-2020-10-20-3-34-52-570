bda5ac126c953839585f5bfea4c6a69d1ca6fad0
Janelle <dominja@oocl.com>
2020-10-21 16:24:36 +0800

    [Employee][Janelle-Charlie][Feature]: added service and repository

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.List;
+
+public class EmployeeService {
+    public List<Employee> getAll() {
+        return null;
+    }
+
+}
