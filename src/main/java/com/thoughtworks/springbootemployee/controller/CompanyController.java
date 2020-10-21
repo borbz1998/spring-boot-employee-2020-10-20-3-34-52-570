05b59f7aebcaa2b0fb0830be0bc36000e2a9885b
Janelle <dominja@oocl.com>
2020-10-21 15:25:31 +0800

    [Employee][Janelle-Charlie][Feature]: added method for getID for company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,4 +15,11 @@ public class CompanyController {
     public List<Company> getAllCompanies() {
         return companies;
     }
+
+    @GetMapping({"/{companyID}"})
+    public Company getByID(@PathVariable Integer companyID) {
+        return companies.stream().filter(company -> company.getCompanyId()
+                .equals(companyID)).findFirst().orElse(null);
+    }
+
 }
