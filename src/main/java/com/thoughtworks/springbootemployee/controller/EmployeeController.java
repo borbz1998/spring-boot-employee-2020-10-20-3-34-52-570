79d902e824926d8d302caee232a5496e006416c6
Janelle <dominja@oocl.com>
2020-10-21 14:40:49 +0800

    [Employee][Janelle-Charlie][Feature]: added method for get specific employee using id

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    private List<Employee> employees =  new ArrayList<>();
+    private List<Employee> employees = new ArrayList<>();
 
-    //    @GetMapping(path = "/{userName}")
     @GetMapping
     public List<Employee> getAll() {
         return employees;
+    }
+
+    @GetMapping({"/{employeeID}"})
+    public Employee get(@PathVariable Integer employeeID) {
+        return employees.stream().filter(employee -> employee.getId()
+                .equals(employeeID)).findFirst().orElse(null);
     }
 }
