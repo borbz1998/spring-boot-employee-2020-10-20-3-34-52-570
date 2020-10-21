869e21c087c4fd94ea6b365b975c36563495c5da
Janelle <dominja@oocl.com>
2020-10-21 16:52:28 +0800

    [Employee][Janelle-Charlie][Feature]: added company as controller

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/companies")
 public class CompanyController {
     private List<Company> companies = new ArrayList<>();
 
